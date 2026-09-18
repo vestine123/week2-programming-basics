4a12da87ed17a76f8313d933eb7f748c9469cbf2
vestineuwimana <vestineuwimana523@gmail.com>
2026-09-18 09:19:01 +0200

    age

--- LabA.js
+++ LabA.js
@@ -1,5 +1,5 @@
 let name = "Vestine";
-let age = 24;
+let age = 22;
 let town = "Kigali";
 let goal = "to become a software developer";
 console.log("My name is " + name + ". I am " + age + " years old. I live in " + town + " and my goal is " + goal + ".");
