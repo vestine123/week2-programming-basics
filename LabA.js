b2a798a11855a57e6ba363b2d943a7466afe5a5c
vestineuwimana <vestineuwimana523@gmail.com>
2026-09-18 09:59:43 +0200

    variables and datatype in javascript

--- LabA.js
+++ LabA.js
@@ -1,6 +1,6 @@
 let name = "Vestine";
-let age = 22;
-let town = "Kigal";
+let age = 25;
+let town = "Kigali";
 let goal = "to become a software developer";
 console.log("My name is " + name + ". I am " + age + " years old. I live in " + town + " and my goal is " + goal + ".");
 
---
+++ labB.js
@@ -0,0 +1,13 @@
+let num1 = 20;
+let num2 = 6;
+
+console.log("Sum: " + (num1 + num2));
+console.log("Difference: " + (num1 - num2));
+console.log("Product: " + (num1 * num2));
+
+if (num2 === 0) {
+    console.log("Cannot divide by zero.");
+} else {
+    console.log("Quotient: " + (num1 / num2));
+    console.log("Remainder: " + (num1 % num2));
+}
