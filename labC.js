4ac2de583ebb270003f5c04b703b658a14ac9577
vestineuwimana <vestineuwimana523@gmail.com>
2026-09-22 14:02:48 +0200

    for loop

---
+++ labC.js
@@ -0,0 +1,15 @@
+let score = 60;
+
+if (score >= 90 && score <= 100) {
+    console.log("Grade: A");
+} else if (score >= 80) {
+    console.log("Grade: B");
+} else if (score >= 70) {
+    console.log("Grade: C");
+} else if (score >= 60) {
+    console.log("Grade: D");
+} else if (score >= 0) {
+    console.log("Grade: F");
+} else {
+    console.log("Invalid score");
+}
---
+++ labD.js
@@ -0,0 +1,5 @@
+let number = 5;
+
+for (let i = 1; i <= 12; i++) {
+    console.log(number + " x " + i + " = " + (number * i));
+}
